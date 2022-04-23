1702ee11d9931d87c2130bab221efbb291d1da69
kopy-kat <kopp.konrad@gmail.com>
2022-04-23 19:33:38 +0200

    updates

--- frontend/pages/new-round.js
+++ frontend/pages/new-round.js
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
 import Head from 'next/head'
 import Link from 'next/link'
-import { rounds } from './data/rounds'
+import { rounds } from '../data/rounds'
 import { useRouter } from 'next/router'
 import { useState, useEffect, useCallback } from 'react'
 import { create } from 'ipfs-http-client';
 const siwe = require('siwe');
 import { ethers } from 'ethers'
-import fs from "fs"
+import axios from 'axios'
 
 const domain = "localhost";
 const origin = "https://localhost/login";
@@ -51,12 +51,13 @@ const Aside = () => (
         animate={{ x: 0 }}
 
   className="hidden sm:flex sm:flex-col">
+  <Link href="/">
     <a
-      href="#"
       className="inline-flex items-center justify-center h-20 w-20 bg-gradient-to-r from-green-500 to-blue-700 hover:bg-blue-500 focus:bg-blue-500 text-white"
     >
       Retro
     </a>
+    </Link>
     <div className="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
       <motion.nav
       initial="hidden"
@@ -64,9 +65,8 @@ const Aside = () => (
         variants={container}
       className="flex flex-col mx-4 my-6 space-y-4">
 
-
+      <Link href="/rounds">
         <a
-          href="/rounds"
           className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-xl"
         >
           <span className="sr-only">Dashboard</span>
@@ -78,16 +78,16 @@ const Aside = () => (
             className="h-6 w-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWdith="2"
               d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
             />
           </svg>
         </a>
+        </Link>
 
         <a
-          href="#"
           className="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-xl"
         >
           <span className="sr-only">Dashboard</span>
@@ -99,9 +99,9 @@ const Aside = () => (
           className="h-6 w-6"
           >
           <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWdith="2"
               d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
           />
           </svg>
@@ -119,15 +119,15 @@ const Aside = () => (
             className="h-6 w-6"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWdith="2"
               d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
             />
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWdith="2"
               d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
             />
           </svg>
@@ -149,9 +149,9 @@ const Header = (props) => (
         className="h-6 w-6"
       >
         <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWdith="2"
           d="M4 6h16M4 12h16M4 18h7"
         />
       </svg>
@@ -195,7 +195,7 @@ const Main = (props) => (
       <div className="row-span-3 col-span-4 bg-white rounded-xl shadow-md">
         <div className="overflow-y-auto p-5">
           <form onSubmit={props.onSubmit}>
-            <div class="grid grid-rows-2 grid-flow-col gap-4">
+            <div className="grid grid-rows-2 grid-flow-col gap-4">
               <div className="flex flex-col">
                 <label className="text-lg ml-1 mb-2">Round name</label>
                 <input type="text" placeholder="Retro" name="roundName" className="border rounded-xl p-2" required></input>
@@ -209,8 +209,8 @@ const Main = (props) => (
                 <input type="number" placeholder="Retro" name="funding" className="border rounded-xl p-2" required></input>
               </div>
               <div className="flex flex-col">
-                <label className="text-lg ml-1 mb-2">Badgeholders (as a CSV file)</label>
-                <input type="file" name="badgeholders" className="mt-2 ml-2" accept=".csv" required></input>
+                <label className="text-lg ml-1 mb-2">Badgeholders (entries on a seperate line)</label>
+                <textarea name="badgeholders" className="border rounded-xl p-2" placeholder="address, Twitter handle" rows="4" cols="50" required></textarea>
               </div>
             </div>
             <div className="flex flex-col items-center mt-4">
@@ -251,32 +251,18 @@ export default function NewRound() {
     }));
   }, []);
 
-
-  function csvJSON(file){
-    const csvfile=fs.readFileSync(file)
-    var lines = csvfile.split("\n")
-    lines.forEach(element => {
-      let contents = element.split(",")
-      return {
-        address: contents[0].trim(),
-        twitter: contents[1].trim()
-      }
-    }); 
-    return lines
-  }
   async function formSubmit(event) {
     event.preventDefault();
     const {roundName, description, funding, badgeholders} = event.target.elements;
-    const res = await ipfs.add(JSON.stringify({
-      roundName: roundName.value,
-      description: description.value
-    }));
-    const ipfsURI = `ipfs://${res.path}`;
-    const metadata = JSON.stringify({
-      ipfsURI: ipfsURI,
-      recipientAddress: recipientAddress.value
-    })
-    const badgeholdermdata = csvJSON(badgeholders)
+    // const res = await ipfs.add(JSON.stringify({
+    //   roundName: roundName.value,
+    //   description: description.value
+    // }));
+    // const ipfsURI = `ipfs://${res.path}`;
+    // const metadata = JSON.stringify({
+    //   ipfsURI: ipfsURI,
+    //   recipientAddress: recipientAddress.value
+    // })
     // create transaction with staking
   }
 
@@ -296,7 +282,7 @@ export default function NewRound() {
 
   useEffect(() => {
     setAddress(window.localStorage.getItem("userAddress"))
-  });
+  }, []);
 
   return (
     <>
