b94a7221225ece95d485e81e804598cb117e775a
jessicapointing <jqpointing@gmail.com>
2022-04-23 15:03:47 +0200

    adding test case

--- retro/test/retro-test.js
+++ retro/test/retro-test.js
@@ -1,5 +1,5 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const { ethers,waffle } = require("hardhat");
 
 describe("Retro", function () {
   it("should deploy", async function () {
@@ -7,5 +7,24 @@ describe("Retro", function () {
     const retro = await Retro.deploy();
     await retro.deployed();
 
+  });
+
+  it("should create a round", async function () {
+    const provider = waffle.provider;
+    const [owner, addr1, addr2, addr3, addr4] = await ethers.getSigners();
+    const Retro = await ethers.getContractFactory("Retro");
+    const retro = await Retro.deploy();
+    await retro.deployed();
+    const badgeHolders = [addr1,addr2];
+    retro.createRound("",badgeHolders,BigInt(10000*(10**18)));
+    retro.nominate(0,"nomination1", addr3);
+    retro.nominate(0,"nomination2", addr4);
+    retro.castVote(0,[50,50]);
+
+    retro.disperseFunds(0);
+    const weiValue = await provider.getBalance(addr3.address)
+    const ethValue = ethers.utils.formatEther(weiValue);
+    const expectedValue = ethers.utils.formatEther(BigInt(10000*(10**18)));
+    expect(ethValue).to.equal(expectedValue);
   });
 });
