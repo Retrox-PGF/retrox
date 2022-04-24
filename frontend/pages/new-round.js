d11f7909625095067c9df4401e72d760a76fdab5
jan-o-e <82890641+jan-o-e@users.noreply.github.com>
2022-04-24 04:12:08 +0100

    Update metadata upload with new contract

--- frontend/pages/new-round.js
+++ frontend/pages/new-round.js
@@ -239,7 +239,7 @@ async function contractInitRound(roundURI, badgeholders, value){
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const signer = provider.getSigner();
   console.log(signer)
-  const retroAddress = "0xf9b9D7dBb7c2a49AB3D021D9aeC8A7C9d7f890AB"
+  const retroAddress = "0x3cAD7cd0d54E0794D5864e9979B21a60E04fDC6b"
   const retroABI = [
     "function createRound(string memory roundURI, address[] memory badgeHolders) public payable"
   ]
