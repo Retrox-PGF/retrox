58ad534ae43f622c43d8c5ff2a19595675877d66
jessicapointing <jqpointing@gmail.com>
2022-04-24 07:35:40 +0200

    deployaed contract

--- retro/test/retro-test.js
+++ retro/test/retro-test.js
@@ -21,18 +21,22 @@ describe("Retro", function () {
     const retro = await Retro.deploy(host);
     await retro.deployed();
     const badgeHolders = [addr1.address,addr2.address];
-
-    await retro.createRound("round1", badgeHolders, {value:ethers.utils.parseEther("0.1")});
+    sleep(10000);
+    await retro.createRound("round1", badgeHolders, {value:ethers.utils.parseEther("0.0001")});
     console.log("createRound");
     sleep(10000);
-    await retro.nominate(0,"nomination1", addr3.address, {value:ethers.utils.parseEther("0.005")});
+    await retro.nominate(0,"nomination1", addr3.address, {value:ethers.utils.parseEther("0.00000005")});
     console.log("nominate1");
-    await retro.nominate(0,"nomination2", addr4.address, {value:ethers.utils.parseEther("0.005")});
+    sleep(10000);
+    await retro.nominate(0,"nomination2", addr4.address, {value:ethers.utils.parseEther("0.00000005")});
     console.log("nominate2");
+    sleep(10000);
     await retro.castVote(0,[50,50]);
     console.log("cast1");
+    sleep(10000);
     await retro.castVote(0,[20,80]);
     console.log("cast2");
+    sleep(10000);
 
     await retro.disperseFunds(0);
     const addr3WeiValue = await provider.getBalance(addr3.address)
@@ -45,9 +49,18 @@ describe("Retro", function () {
     const nom1 = await retro.getNominationData(0,0);
     const nom2 = await retro.getNominationData(0,1);
     const round1 = await retro.getRoundData(0);
+    //const amount1 = await retro.getAmountData(0);
 
+    console.log(round1);
+    console.log("eth values");
     console.log(addr3EthValue);
     console.log(addr4EthValue);
+
+    console.log("amount");
+    //console.log(amount1);
+
+    console.log(nom1);
+    console.log(nom2);
 
     // expect(addr3EthValue > (addr3ExpectedValue-2));
     // expect(addr4EthValue > (addr4ExpectedValue-2));
