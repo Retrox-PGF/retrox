aa55af129f753520ffbd224dddea86380dd027f8
kopy-kat <kopp.konrad@gmail.com>
2022-04-24 05:26:16 +0200

    frontend

--- frontend/pages/_components/ChartModal.js
+++ frontend/pages/_components/ChartModal.js
@@ -27,8 +27,55 @@ const ChartModal = (props) => {
     labels: [],
     datasets: [{
       data: [],
-      backgroundColor: [],
-      hoverBackgroundColor: [],
+      borderWidth: 0.5,
+      backgroundColor:  [
+      "#0700CF",
+      "#1900CF",
+      "#2A00CE",
+      "#3C00CE",
+      "#4D00CE",
+      "#5E00CD",
+      "#7000CD",
+      "#8100CD",
+      "#9200CC",
+      "#A900D3",
+      "#8900CA",
+      "#6B00C1",
+      "#4F00B8",
+      "#3500AF",
+      "#1E00A6",
+      "#09009D",
+      "#000BA2",
+      "#0020A7",
+      "#0036AC",
+      "#004EB1",
+      "#0066B6",
+      "#0080BA",
+      "#0098BF"],
+      hoverBackgroundColor: [
+        "#0700CF80",
+        "#1900CF80",
+        "#2A00CE80",
+        "#3C00CE80",
+        "#4D00CE80",
+        "#5E00CD80",
+        "#7000CD80",
+        "#8100CD80",
+        "#9200CC80",
+        "#A900D380",
+        "#8900CA80",
+        "#6B00C180",
+        "#4F00B880",
+        "#3500AF80",
+        "#1E00A680",
+        "#09009D80",
+        "#000BA280",
+        "#0020A780",
+        "#0036AC80",
+        "#004EB180",
+        "#0066B680",
+        "#0080BA80",
+        "#0098BF80"],
       options: {
         scales: {
           y: {
@@ -49,8 +96,8 @@ const ChartModal = (props) => {
         // console.log(Object.entries(voteDataCopy).length - 4)
         // returnData.labels.push(legend[key]);
         returnData.labels.push(key);
-        returnData.datasets[0].backgroundColor.push(createColor());
-        returnData.datasets[0].hoverBackgroundColor.push(createColor());
+        // returnData.datasets[0].backgroundColor.push(createColor());
+        // returnData.datasets[0].hoverBackgroundColor.push(createColor());
       }
     }
     return returnData;
