10f1c914fa5864e555e938e94fc07b9ffd0241b8
Fernando Almansa <fernalmansa@gmail.com>
2022-04-24 05:23:38 +0200

    New funding modal

---
+++ frontend/pages/_components/FundingModal.js
@@ -0,0 +1,27 @@
+const FundingModal = (props) => {
+  
+    return (
+      <>
+        <div
+          className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none py-5" onClick={() => {props.close()}}
+        >
+          <div className="relative w-auto my-6 mx-auto max-w-full max-h-screen my-5 mx-5 py-5">
+            <div className="border rounded-2xl shadow-lg relative bg-white flex flex-col w-full outline-none focus:outline-none" onClick={e => {e.stopPropagation();}}>
+              <div className="relative px-20 py-8 flex-auto mx-auto text-center text-xl">
+                <h2 className="font-bold text-3xl my-4 text-blue-600">Funding Information</h2>
+                <text>This round is funded by Optimism's donation of $1 million of Optimism Network Fees</text>
+                <div></div>
+                <a href={'https://medium.com/ethereum-optimism/retropgf-experiment-1-1-million-dollars-for-public-goods-f7e455cbdca'} target="_blank" className="m-1 text-blue-500 hover:text-blue-700">
+                    Article
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      </>
+    );
+  };
+  
+  export default FundingModal;
+  
