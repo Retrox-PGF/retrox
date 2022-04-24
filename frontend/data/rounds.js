02fc7a3b4b0d28c2fe7cf4dc452cf403efbef8f7
kopy-kat <kopp.konrad@gmail.com>
2022-04-24 06:52:18 +0200

    nice ones

--- frontend/data/rounds.js
+++ frontend/data/rounds.js
@@ -1,8 +1,7 @@
 export const rounds = [
-  {name: 'My round', id: 1},
-  {name: 'My other round', id: 2},
-  {name: 'Optimism round', id: 3},
-  {name: 'i like parks round', id: 4},
-  {name: 'My private round', id: 5},
-  {name: 'Anotehr round', id: 6},
+  {name: 'Optimism Initial Round', id: 1, date:'5/10/2021', category:'Open'},
+  {name: 'Ethereum Foundation', id: 3, date:'19/1/2022', category:'Open'},
+  {name: 'Gates Foundation', id: 4, date:'23/4/2022', category:'Pandemic prevention and response'},
+  {name: 'David Koch Foundation', id: 5, date:'24/4/2022', category:'Sustainability and environmental protection'},
+  {name: 'OpenAI AI Saftey Round', id: 2, date:'25/4/2022', category:'AI Saftey'},
 ];
--- frontend/pages/data/rounds.js
+++
@@ -1,8 +0,0 @@
-export const rounds = [
-  {name: 'My round', id: 1},
-  {name: 'My other round', id: 2},
-  {name: 'Optimism round', id: 3},
-  {name: 'i like parks round', id: 4},
-  {name: 'My private round', id: 5},
-  {name: 'Another round', id: 6},
-];
