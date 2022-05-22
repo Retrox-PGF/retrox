c71ba74d4d053a9aafa331e542401c8d23e2a269
jan-o-e <82890641+jan-o-e@users.noreply.github.com>
2022-05-22 12:33:03 +0100

    fetch ipfs metadata with node-fetch
    need to include throw err for non valid url, which should push nothing into dictionary

--- frontend/lib/getRounds.js
+++ frontend/lib/getRounds.js
@@ -1,5 +1,13 @@
 import { ethers } from "ethers"
 import { deployed_address } from '../contract_config.js';
+import fetch from 'node-fetch'
+
+
+//function to convert uri from contract to URL which can be called to get JSON metadata
+function uriToURL(uri){
+    return `https://ipfs.infura.io/ipfs/${uri.slice(7)}`
+}
+
 
 export async function getRounds() {
     const provider = new ethers.providers.JsonRpcProvider(process.env.INFURA_URL);
@@ -14,10 +22,16 @@ export async function getRounds() {
     const numRounds = (await retroContract.getNextRoundNum()).toNumber();
 
     let rounds = []
-    for (let i = 0; i < numRounds; i++) {
+    for (let i = 0; i <= numRounds; i++) {
         const round = await retroContract.getRoundData(i);
+
+        const ipfsHTTPS = uriToURL(round[0]);
+        const ipfsresponse = await fetch(ipfsHTTPS);
+        const metadata = await ipfsresponse.json()
+
         rounds.push({
             roundURI: round[0],
+            roundName: metadata,
             startBlockTimestamp: round[1].toNumber(),
             fundsCommitted: ethers.utils.formatEther(round[2]),
             nominationCounter: round[3].toNumber(),
